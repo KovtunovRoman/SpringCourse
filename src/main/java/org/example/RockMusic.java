88d12b4ac787b5386c0a4a8eccc6116f8066b570
Roman Kovtunov <ro.kovtunov@mail.ru>
2022-08-23 17:28:55 +0300

    Sixth Lesson
    Usage patterns(singleton and factory-method)

--- src/main/java/org/example/RockMusic.java
+++ src/main/java/org/example/RockMusic.java
@@ -1,6 +1,20 @@
 package org.example;
 
 public class RockMusic implements Music{
+
+    private RockMusic(){}
+
+    public static RockMusic getRockMusicObject(){
+        return new RockMusic();
+    }
+    public void doMyInit(){
+        System.out.println("Initialization is starting");
+    }
+
+    public void doMyDestroy(){
+        System.out.println("Destroy bean");
+    }
+
     @Override
     public String getSong() {
         return "Rock Music";
--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -6,10 +6,16 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
+        RockMusic rockMusic = context.getBean("musicBean", RockMusic.class);
+        RockMusic secondRockMusic = context.getBean("musicBean", RockMusic.class);
+
+        System.out.println(secondRockMusic.getSong());
+        System.out.println(rockMusic.getSong());
+
         //Music music = context.getBean("musicBean", Music.class);
         //MusicPlayer musicPlayer = new MusicPlayer(music);
 
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        /*MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
         musicPlayer.playMusic();
@@ -21,7 +27,7 @@ public class TestSpring {
         System.out.println(secondMusicPlayer.getName());
         System.out.println(secondMusicPlayer.getVolume());
         System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
+        System.out.println(musicPlayer.getVolume());*/
 
         context.close();
     }
