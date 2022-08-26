3de997cf528305db9fe3c46f3872de487700739b
Roman Kovtunov <ro.kovtunov@mail.ru>
2022-08-26 19:03:09 +0300

    Tenth Lesson
    @Scope, @Value, @PostConstruct, @PreDestroy

--- src/main/java/org/example/RockMusic.java
+++ src/main/java/org/example/RockMusic.java
@@ -1,9 +1,22 @@
 package org.example;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
+
 public class RockMusic implements Music{
+    @PostConstruct
+    public void doMyInit(){
+        System.out.println("Initialization start");
+    }
+    @PreDestroy
+    public void doMyDestroy(){
+        System.out.println("Destroy start");
+    }
     @Override
     public String getSong() {
         return "Rock Music";
--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -10,7 +10,17 @@ public class TestSpring {
         musicPlayer.playMusic();
         context.close();*/
 
-        Computer computer = context.getBean("computer", Computer.class);
-        System.out.println(computer);
+        /*Computer computer = context.getBean("computer", Computer.class);
+        System.out.println(computer);*/
+
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
+
+        RockMusic rockMusic1 = context.getBean("rockMusic", RockMusic.class);
+        RockMusic rockMusic2 = context.getBean("rockMusic", RockMusic.class);
+
+        System.out.println(rockMusic2 == rockMusic1);
+        context.close();
     }
 }
